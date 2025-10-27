79aa10c4126789e5c6699b20b2044b2559d80da9
Kyleeg0619 <Kyleeg0619@gmail.com>
2025-10-27 11:28:04 -0400

    install laravel

---
+++ soft-project/resources/views/template.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Template</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://use.typekit.net/bqe7agd.css">
+</head>
+<body>
+    <nav>
+        <div class="logo-title">
+            <img id="logo" src="" alt="sample-logo">
+            <a href="" id="company-title"><h1>Sample Title</h1></a>
+        </div>
+        <div class="nav-links">
+            <a href="">Home</a>
+            <a href="">Register</a>
+            <a href="">Courses</a>
+            <a href="">Majors</a>
+        </div>
+    </nav>
+    <footer>
+<p>sample footer</p>
+    </footer>
+</body>
+</html>
