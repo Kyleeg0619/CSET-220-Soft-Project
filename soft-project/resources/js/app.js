2f430ea27764861568ce3afc86dd0388cf769e54
Kimar <kartermendenhallwe@gmail.com>
2025-11-07 12:07:08 -0500

    bug fixing

--- soft-project/resources/js/app.js
+++
@@ -1,9 +0,0 @@
-import './bootstrap';
-
-function showMenu() {
-    document.getElementById("menu-icon").addEventListener('click',function(){
-        document.querySelector('.nav-links').classList.toggle('visible');
-    });
-}
-
-showMenu();
--- soft-project/resources/js/menu.js
+++
@@ -1,7 +0,0 @@
-function showMenu() {
-    document.getElementById("menu-icon").addEventListener('click',function(){
-        document.querySelector('.nav-links').classList.toggle('visible');
-    });
-}
-
-showMenu();
